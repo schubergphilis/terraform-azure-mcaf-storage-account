e081cc2ce3bad5738e58a0155e6cb8f6c8112743
Nelesh Goel <ngoel@schubergphilis.com>
2025-02-24 17:25:46 +0100

    fix: blob storage backup

--- variables.tf
+++ variables.tf
@@ -193,12 +193,12 @@ variable "network_configuration" {
 }
 
 variable "blob_storage_backup" {
-  description = "blob storage backup policy settings for the storage account. Defaults to null which does not set any blob storage backup policy"
-  type = object({
+  description = "Map of blob storage backup policy settings for the storage account. Defaults to an empty map which does not set any blob storage backup policy."
+  type = map(object({
     name             = optional(string, null)
     backup_vault_id  = optional(string, null)
     backup_policy_id = optional(string, null)
-  })
+  }))
   default = {}
 }
 
