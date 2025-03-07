7c32c37e44bc09bbaeba966d219917f3e092a58e
Niek van Raaij <nvanraaij@schubergphilis.com>
2025-03-07 15:31:13 +0100

    chore: support lower version of TF

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.9"
+  required_version = ">= 1.8.5"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4, < 5.0"
+      version = "~> 4.0.0"
     }
   }
 }
